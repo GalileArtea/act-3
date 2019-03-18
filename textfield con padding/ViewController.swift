618b12b2901f3d873c0ea64ce785959b4ed9b1e3
Luna Galilea <al02797904@tecmilenio.mx>
2019-03-17 18:36:14 -0600

    Initial Commit

---
+++ textfield con padding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  textfield con padding
+//
+//  Created by BLUE BLUE on 3/17/19.
+//  Copyright © 2019 Luna Galilea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ textfield con paddingTests/textfield_con_paddingTests.swift
@@ -0,0 +1,34 @@
+//
+//  textfield_con_paddingTests.swift
+//  textfield con paddingTests
+//
+//  Created by BLUE BLUE on 3/17/19.
+//  Copyright © 2019 Luna Galilea. All rights reserved.
+//
+
+import XCTest
+@testable import textfield_con_padding
+
+class textfield_con_paddingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
